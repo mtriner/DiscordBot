757a78de7f630e72f79b583e37f109d894d28400
mtriner <36250134+mtriner@users.noreply.github.com>
2022-07-03 13:59:50 -0400

    Init
    initial creation and setup

---
+++ bot.py
@@ -0,0 +1,15 @@
+from gettext import find
+import discord
+import config
+
+TOKEN = config.DISCORD_TOKEN
+
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as {0}!'.format(self.user))
+
+    async def on_message(self, message):
+        print('Message from {0.author}: {0.content}'.format(message))
+
+client = MyClient()
+client.run(TOKEN)
---
+++ config.py
@@ -0,0 +1,3 @@
+# .gitignore
+# config.py
+DISCORD_TOKEN = 'OTkzMjAxNTQxMzQxMjQ5NjQ2.Gt43tH.spkdWxbNBpI3neMq4qzdF9yLa9VK7vCG1hhusg'
